416b92a31d4d79fb7fae8eb3be2504420d2d2c9c
Weizheng <zhou66.ustcz@gmail.com>
2017-03-04 19:50:17 -0800

    m

--- three_sum.py
+++ three_sum.py
@@ -1,5 +1,8 @@
 # https://leetcode.com/problems/3sum/?tab=Description
 
+import collections
+
+
 class Solution(object):
   """
   >>> Solution().threeSum([-1, 0, 1])
@@ -12,25 +15,39 @@ class Solution(object):
     if not numbers or len(numbers) < 3:
       return []
 
-    index_by_number = {
-        numbers[i]: i for i in range(len(numbers))
-    }
+    index_by_number = collections.defaultdict(list)
+
+    for i in range(len(numbers)):
+      index_by_number[numbers[i]].append(i)
+
     solutions = []
     seen_solution_set = set()
     for i in range(len(numbers) - 2):
       for j in range(i+1, len(numbers) - 1):
         two_sum = numbers[i] + numbers[j]
-        third_index = index_by_number.get(-two_sum, None)
-        if third_index and third_index not in [i, j]:
-          # print i, j, third_index
+        third_indices = index_by_number.get(-two_sum)
+
+        third_index = self._findThirdIndex(third_indices, i, j)
+        if third_index:
           solution = sorted([numbers[i], numbers[j], numbers[third_index]])
           if tuple(solution) not in seen_solution_set:
             solutions.append(solution)
             seen_solution_set.add(tuple(solution))
     return solutions
+
+  def _findThirdIndex(self, third_indices, i, j):
+    if not third_indices:
+      return None
+    elif len(third_indices) == 1 and third_indices[0] > j:
+      return third_indices[0]
+    elif len(third_indices) == 2 and third_indices[1] > j:
+      return third_indices[1]
+    elif len(third_indices) > 2:
+      return third_indices[2]
+    return None
     
 
 if __name__ == '__main__':
-  print Solution().threeSum([-1, 0, 1, 2, -1, -4])
-  # import doctest
-  # doctest.testmod()
+  # print Solution().threeSum([-1, 0, 1, 2, -1, -4])
+  import doctest
+  doctest.testmod()
